edbe68192251acd03ee1ae16c334372542e2b936
IndianRana <indianrana007@gmail.com>
2025-04-21 15:54:57 +0530

    first commit

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './login.css'
+import assets from '../../assets/assets'
+
+const login = () => {
+  return (
+    <div className='login'>
+        <img src={assets.logo_big} alt="" className="logo" />
+    </div>
+  )
+}
+
+export default login
